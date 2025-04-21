dc9543a8c3412c1da94342076ff492592cb2f89d
kanisan09 <194599381+kanisan09@users.noreply.github.com>
2025-04-21 15:47:07 +0900

    更新

--- app/Http/Controllers/MemberController.php
+++ app/Http/Controllers/MemberController.php
@@ -2,9 +2,23 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Member;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Redirect;
 
 class MemberController extends Controller
 {
-    //
+    public function edit(Member $member){
+        return view('members.edit', compact('member'));
+    }
+
+    public function update(Request $request, Member $member){
+        $request->validate([
+            'name' => 'required|max255',
+        ]);
+
+        $member->update(['name' => $request->name]);
+
+        return Redirect::route('members.index')->with('message', '更新しました！');
+    }
 }
