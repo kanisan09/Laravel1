bff1eaa81d78059a68388e656f682dea402ab0a6
kanisan09 <194599381+kanisan09@users.noreply.github.com>
2025-04-23 14:41:44 +0900

    更新

--- resources/views/livewire/member.blade.php
+++ resources/views/livewire/member.blade.php
@@ -2,26 +2,32 @@
 
 <div class="mx-auto max-w-3xl">
     @if ($message)
-        <div style="background-color: #d4edda; border: 1px solid #a7f7ba; color: #155724; padding: 0.75rem 1rem; border-radius: 0.25rem; position: relative;" role="alert">
+        <div style="background-color: #d4edda; border: 1px solid #a7f7ba; color: #155724; padding: 0.75rem 1rem; border-radius: 0.25rem; position: relative;"
+            role="alert">
             <strong class="font-bold">成功！</strong>
             <span class="block sm:inline">{{ $message }}</span>
             <button wire:click="$set('message', null)" class="absolute top-0 bottom-0 right-0 px-4 py-3">
-                <svg class="fill-current h-6 w-6 text-green-500" role="button" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
+                <svg class="fill-current h-6 w-6 text-green-500" role="button" xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 20 20">
                     <title>閉じる</title>
-                    <path fill-rule="evenodd" d="M14.348 14.849a1.2 1.2 0 0 1-1.697 0L10 11.546l-2.651 3.303a1.2 1.2 0 0 1-1.697-1.697l3.303-2.651-3.303-2.651a1.2 1.2 0 0 1 1.697-1.697L10 8.546l2.651-3.303a1.2 1.2 0 0 1 1.697 1.697l-3.303 2.651 3.303 2.651a1.2 1.2 0 0 1 0 1.697z" />
+                    <path fill-rule="evenodd"
+                        d="M14.348 14.849a1.2 1.2 0 0 1-1.697 0L10 11.546l-2.651 3.303a1.2 1.2 0 0 1-1.697-1.697l3.303-2.651-3.303-2.651a1.2 1.2 0 0 1 1.697-1.697L10 8.546l2.651-3.303a1.2 1.2 0 0 1 1.697 1.697l-3.303 2.651 3.303 2.651a1.2 1.2 0 0 1 0 1.697z" />
                 </svg>
             </button>
         </div>
     @endif
 
     @if ($error)
-        <div style="background-color: #f8d7da; border: 1px solid #f5c6cb; color: #721c24; padding: 0.75rem 1rem; border-radius: 0.25rem; position: relative;" role="alert">
+        <div style="background-color: #f8d7da; border: 1px solid #f5c6cb; color: #721c24; padding: 0.75rem 1rem; border-radius: 0.25rem; position: relative;"
+            role="alert">
             <strong class="font-bold">エラー！</strong>
             <span class="block sm:inline">{{ $error }}</span>
             <button wire:click="$set('error', null)" class="absolute top-0 bottom-0 right-0 px-4 py-3">
-                <svg class="fill-current h-6 w-6 text-red-500" role="button" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
+                <svg class="fill-current h-6 w-6 text-red-500" role="button" xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 20 20">
                     <title>閉じる</title>
-                    <path fill-rule="evenodd" d="M14.348 14.849a1.2 1.2 0 0 1-1.697 0L10 11.546l-2.651 3.303a1.2 1.2 0 0 1-1.697-1.697l3.303-2.651-3.303-2.651a1.2 1.2 0 0 1 1.697-1.697L10 8.546l2.651-3.303a1.2 1.2 0 0 1 1.697 1.697l-3.303 2.651 3.303 2.651a1.2 1.2 0 0 1 0 1.697z" />
+                    <path fill-rule="evenodd"
+                        d="M14.348 14.849a1.2 1.2 0 0 1-1.697 0L10 11.546l-2.651 3.303a1.2 1.2 0 0 1-1.697-1.697l3.303-2.651-3.303-2.651a1.2 1.2 0 0 1 1.697-1.697L10 8.546l2.651-3.303a1.2 1.2 0 0 1 1.697 1.697l-3.303 2.651 3.303 2.651a1.2 1.2 0 0 1 0 1.697z" />
                 </svg>
             </button>
         </div>
@@ -29,27 +35,33 @@
 
     {{-- 新規追加フォーム --}}
     @if (!$editingMemberId)
-        <form wire:submit.prevent="save" class="flex justify-end">
-            <div class="mb-4 w-full">
-                <label for="name" class="block text-gray-700 text-sm font-bold mb-2">{{ __('名前') }}</label>
-                <input id="name" type="text" wire:model="name" class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline">
+        <form wire:submit.prevent="save" class="m-1">
+            <div class="m-3 ">
+                <label for="name" class="block text-gray-700 text-sm font-bold mb-2">{{ __('新規登録') }}</label>
+                <div class="flex items-center">
+                    <input id="name" type="text" wire:model="name"
+                        class="shadow appearance-none border rounded w-full py-4 px-6 text-gray-700 leading-tight focus:outline-none focus:shadow-outline">
+                    <button type="submit"
+                        class="bg-indigo-500 hover:bg-indigo-700 text-white font-bold py-1 px-1 rounded focus:outline-none focus:shadow-outline w-1/6 ml-2 py-3 m-2">
+                        {{ __('保存') }}
+                    </button>
+                </div>
             </div>
-            <button type="submit" class="bg-indigo-500 hover:bg-indigo-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline w-1/4 ml-4">
-                {{ __('保存') }}
-            </button>
         </form>
     @endif
 
     <div class="">
-        <h2 class="p-4">一覧</h2>
+        <h2 class="p-4">会員一覧</h2>
         @foreach ($members as $member)
             <div style="display: flex; align-items: center;" class="flex items-center mb-2 bg-white rounded-md p-4">
                 <span>{{ $member->name }}</span>
                 <div class="ml-auto">
-                    <a href="{{ route('members.edit', $member->id) }}" class="inline-flex items-center bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline mr-2">
+                    <a href="{{ route('members.edit', $member->id) }}"
+                        class="inline-flex items-center bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline mr-2">
                         編集
                     </a>
-                    <button wire:click="deleteMember({{ $member->id }})" class="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline">
+                    <button wire:click="deleteMember({{ $member->id }})"
+                        class="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline">
                         削除
                     </button>
                 </div>
--- resources/views/navigation-menu.blade.php
+++ resources/views/navigation-menu.blade.php
@@ -1,51 +1,59 @@
 <nav x-data="{ open: false }" class="bg-white border-b border-gray-100">
-    <!-- Primary Navigation Menu -->
     <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div class="flex justify-between h-16">
             <div class="flex">
-                <!-- Logo -->
                 <div class="shrink-0 flex items-center">
                     <a href="{{ route('dashboard') }}">
                         <x-application-mark class="block h-9 w-auto" />
                     </a>
                 </div>
 
-                <!-- Navigation Links -->
                 <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
                     <x-nav-link href="{{ route('dashboard') }}" :active="request()->routeIs('dashboard')">
                         {{ __('Dashboard') }}
+                    </x-nav-link>
+                    <x-nav-link href="{{ url('/admin/member') }}" :active="request()->routeIs('admin/member')">
+                        {{ __('会員一覧') }}
+                    </x-nav-link>
+                    <x-nav-link href="{{ url('/admin/member') }}" :active="request()->routeIs('admin/member')">
+                        <img src="{{ asset('img/inu.jpg') }}" alt="いぬ" class="h-12 w-auto">
+                    </x-nav-link>
+                    <x-nav-link href="{{ url('/admin/member') }}" :active="request()->routeIs('admin/member')">
+                        <img src="{{ asset('img/tanuki.png') }}" alt="たぬき" class="h-12 w-auto">
                     </x-nav-link>
                 </div>
             </div>
 
             <div class="hidden sm:flex sm:items-center sm:ms-6">
-                <!-- Teams Dropdown -->
                 @if (Laravel\Jetstream\Jetstream::hasTeamFeatures())
                     <div class="ms-3 relative">
                         <x-dropdown align="right" width="60">
                             <x-slot name="trigger">
                                 <span class="inline-flex rounded-md">
                                     <button type="button" class="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-gray-500 bg-white hover:text-gray-700 focus:outline-none focus:bg-gray-50 active:bg-gray-50 transition ease-in-out duration-150">
-                                        {{ Auth::user()->currentTeam->name }}
-
-                                        <svg class="ms-2 -me-0.5 size-4" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor">
-                                            <path stroke-linecap="round" stroke-linejoin="round" d="M8.25 15L12 18.75 15.75 15m-7.5-6L12 5.25 15.75 9" />
-                                        </svg>
+                                        @if (Auth::user()->currentTeam)
+                                            {{ Auth::user()->currentTeam->name }}
+                                            <svg class="ms-2 -me-0.5 size-4" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor">
+                                                <path stroke-linecap="round" stroke-linejoin="round" d="M8.25 15L12 18.75 15.75 15m-7.5-6L12 5.25 15.75 9" />
+                                            </svg>
+                                        @else
+                                            {{ __('No Team') }}
+                                        @endif
                                     </button>
                                 </span>
                             </x-slot>
 
                             <x-slot name="content">
                                 <div class="w-60">
-                                    <!-- Team Management -->
                                     <div class="block px-4 py-2 text-xs text-gray-400">
                                         {{ __('Manage Team') }}
                                     </div>
 
-                                    <!-- Team Settings -->
-                                    <x-dropdown-link href="{{ route('teams.show', Auth::user()->currentTeam->id) }}">
-                                        {{ __('Team Settings') }}
-                                    </x-dropdown-link>
+                                    @if (Auth::user()->currentTeam)
+                                        <x-dropdown-link href="{{ route('teams.show', Auth::user()->currentTeam->id) }}">
+                                            {{ __('Team Settings') }}
+                                        </x-dropdown-link>
+                                    @endif
 
                                     @can('create', Laravel\Jetstream\Jetstream::newTeamModel())
                                         <x-dropdown-link href="{{ route('teams.create') }}">
@@ -53,7 +61,6 @@
                                         </x-dropdown-link>
                                     @endcan
 
-                                    <!-- Team Switcher -->
                                     @if (Auth::user()->allTeams()->count() > 1)
                                         <div class="border-t border-gray-200"></div>
 
@@ -71,19 +78,28 @@
                     </div>
                 @endif
 
-                <!-- Settings Dropdown -->
                 <div class="ms-3 relative">
                     <x-dropdown align="right" width="48">
                         <x-slot name="trigger">
-                            @if (Laravel\Jetstream\Jetstream::managesProfilePhotos())
-                                <button class="flex text-sm border-2 border-transparent rounded-full focus:outline-none focus:border-gray-300 transition">
-                                    <img class="size-8 rounded-full object-cover" src="{{ Auth::user()->profile_photo_url }}" alt="{{ Auth::user()->name }}" />
-                                </button>
+                            @if (Auth::user())
+                                @if (Laravel\Jetstream\Jetstream::managesProfilePhotos())
+                                    <button class="flex text-sm border-2 border-transparent rounded-full focus:outline-none focus:border-gray-300 transition">
+                                        <img class="size-8 rounded-full object-cover" src="{{ Auth::user()->profile_photo_url }}" alt="{{ Auth::user()->name }}" />
+                                    </button>
+                                @else
+                                    <span class="inline-flex rounded-md">
+                                        <button type="button" class="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-gray-500 bg-white hover:text-gray-700 focus:outline-none focus:bg-gray-50 active:bg-gray-50 transition ease-in-out duration-150">
+                                            {{ Auth::user()->name }}
+                                            <svg class="ms-2 -me-0.5 size-4" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor">
+                                                <path stroke-linecap="round" stroke-linejoin="round" d="M19.5 8.25l-7.5 7.5-7.5-7.5" />
+                                            </svg>
+                                        </button>
+                                    </span>
+                                @endif
                             @else
                                 <span class="inline-flex rounded-md">
                                     <button type="button" class="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-gray-500 bg-white hover:text-gray-700 focus:outline-none focus:bg-gray-50 active:bg-gray-50 transition ease-in-out duration-150">
-                                        {{ Auth::user()->name }}
-
+                                        {{ __('Guest') }}
                                         <svg class="ms-2 -me-0.5 size-4" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor">
                                             <path stroke-linecap="round" stroke-linejoin="round" d="M19.5 8.25l-7.5 7.5-7.5-7.5" />
                                         </svg>
@@ -93,7 +109,6 @@
                         </x-slot>
 
                         <x-slot name="content">
-                            <!-- Account Management -->
                             <div class="block px-4 py-2 text-xs text-gray-400">
                                 {{ __('Manage Account') }}
                             </div>
@@ -110,12 +125,11 @@
 
                             <div class="border-t border-gray-200"></div>
 
-                            <!-- Authentication -->
                             <form method="POST" action="{{ route('logout') }}" x-data>
                                 @csrf
 
                                 <x-dropdown-link href="{{ route('logout') }}"
-                                         @click.prevent="$root.submit();">
+                                                     @click.prevent="$root.submit();">
                                     {{ __('Log Out') }}
                                 </x-dropdown-link>
                             </form>
@@ -124,7 +138,6 @@
                 </div>
             </div>
 
-            <!-- Hamburger -->
             <div class="-me-2 flex items-center sm:hidden">
                 <button @click="open = ! open" class="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:bg-gray-100 focus:text-gray-500 transition duration-150 ease-in-out">
                     <svg class="size-6" stroke="currentColor" fill="none" viewBox="0 0 24 24">
@@ -136,7 +149,6 @@
         </div>
     </div>
 
-    <!-- Responsive Navigation Menu -->
     <div :class="{'block': open, 'hidden': ! open}" class="hidden sm:hidden">
         <div class="pt-2 pb-3 space-y-1">
             <x-responsive-nav-link href="{{ route('dashboard') }}" :active="request()->routeIs('dashboard')">
@@ -144,23 +156,27 @@
             </x-responsive-nav-link>
         </div>
 
-        <!-- Responsive Settings Options -->
         <div class="pt-4 pb-1 border-t border-gray-200">
             <div class="flex items-center px-4">
-                @if (Laravel\Jetstream\Jetstream::managesProfilePhotos())
-                    <div class="shrink-0 me-3">
-                        <img class="size-10 rounded-full object-cover" src="{{ Auth::user()->profile_photo_url }}" alt="{{ Auth::user()->name }}" />
+                @if (Auth::user())
+                    @if (Laravel\Jetstream\Jetstream::managesProfilePhotos())
+                        <div class="shrink-0 me-3">
+                            <img class="size-10 rounded-full object-cover" src="{{ Auth::user()->profile_photo_url }}" alt="{{ Auth::user()->name }}" />
+                        </div>
+                    @endif
+
+                    <div>
+                        <div class="font-medium text-base text-gray-800">{{ Auth::user()->name }}</div>
+                        <div class="font-medium text-sm text-gray-500">{{ Auth::user()->email }}</div>
+                    </div>
+                @else
+                    <div>
+                        <div class="font-medium text-base text-gray-800">{{ __('Guest') }}</div>
                     </div>
                 @endif
-
-                <div>
-                    <div class="font-medium text-base text-gray-800">{{ Auth::user()->name }}</div>
-                    <div class="font-medium text-sm text-gray-500">{{ Auth::user()->email }}</div>
-                </div>
             </div>
 
             <div class="mt-3 space-y-1">
-                <!-- Account Management -->
                 <x-responsive-nav-link href="{{ route('profile.show') }}" :active="request()->routeIs('profile.show')">
                     {{ __('Profile') }}
                 </x-responsive-nav-link>
@@ -171,17 +187,15 @@
                     </x-responsive-nav-link>
                 @endif
 
-                <!-- Authentication -->
                 <form method="POST" action="{{ route('logout') }}" x-data>
                     @csrf
 
                     <x-responsive-nav-link href="{{ route('logout') }}"
-                                   @click.prevent="$root.submit();">
+                                         @click.prevent="$root.submit();">
                         {{ __('Log Out') }}
                     </x-responsive-nav-link>
                 </form>
 
-                <!-- Team Management -->
                 @if (Laravel\Jetstream\Jetstream::hasTeamFeatures())
                     <div class="border-t border-gray-200"></div>
 
@@ -189,10 +203,11 @@
                         {{ __('Manage Team') }}
                     </div>
 
-                    <!-- Team Settings -->
-                    <x-responsive-nav-link href="{{ route('teams.show', Auth::user()->currentTeam->id) }}" :active="request()->routeIs('teams.show')">
-                        {{ __('Team Settings') }}
-                    </x-responsive-nav-link>
+                    @if (Auth::user()->currentTeam)
+                        <x-responsive-nav-link href="{{ route('teams.show', Auth::user()->currentTeam->id) }}" :active="request()->routeIs('teams.show')">
+                            {{ __('Team Settings') }}
+                        </x-responsive-nav-link>
+                    @endif
 
                     @can('create', Laravel\Jetstream\Jetstream::newTeamModel())
                         <x-responsive-nav-link href="{{ route('teams.create') }}" :active="request()->routeIs('teams.create')">
@@ -200,7 +215,6 @@
                         </x-responsive-nav-link>
                     @endcan
 
-                    <!-- Team Switcher -->
                     @if (Auth::user()->allTeams()->count() > 1)
                         <div class="border-t border-gray-200"></div>
 
