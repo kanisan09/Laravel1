648e184e20b341a1b9d2fdc9fe9ea7ab71932279
kanisan09 <194599381+kanisan09@users.noreply.github.com>
2025-04-23 16:59:17 +0900

    更新

--- resources/views/self_introduction.blade.php
+++ resources/views/self_introduction.blade.php
@@ -10,11 +10,12 @@
             <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
                 <div class="p-6 sm:px-20 bg-white border-b border-gray-200">
                     <h3 class="text-lg font-bold">
-                        {{ __('はじめまして')}}
+                        {{ __('名前：甲斐詩織（1997.01.23~）')}}
                     </h3>
                     <p class="mt-4">
-                        {{ __('自己紹介文')}}
+                        {{ __('スキル')}}
                     </p>
+                    <p>{{ __(('◇laravel：まだまだ'))}}</p>
                 </div>
             </div>
         </div>
